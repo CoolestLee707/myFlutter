3165f75e246b8635772e06b1d13209a6fcc6d7b7
lichuanmin <Chuanmin.Li-DAJ@YumChina.com>
2020-05-05 16:26:30 +0800

    update environment

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/daoj/Desktop/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/daoj/Desktop/CoolestLee707/Flutter/myflutter"
-export "FLUTTER_TARGET=/Users/daoj/Desktop/CoolestLee707/Flutter/myflutter/lib/main.dart"
+export "FLUTTER_ROOT=/Users/daojia/Desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/daojia/Desktop/CoolestLee707/Flutter/myflutter"
+export "FLUTTER_TARGET=/Users/daojia/Desktop/CoolestLee707/Flutter/myflutter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/daoj/Desktop/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/daojia/Desktop/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "TRACK_WIDGET_CREATION=true"
